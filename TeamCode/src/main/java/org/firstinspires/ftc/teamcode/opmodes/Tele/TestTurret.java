7696f1e2cbefdc7bb7ea3b6bb3c91585cfb85a6c
Malek Swilam <malekgswilam@gmail.com>
2026-01-02 14:17:46 -0500

    servo test turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele/TestTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele/TestTurret.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode.opmodes.Tele;
 
-public class TestTurretServos {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class TestTurret extends OpMode {
+    private Servo servo2, servo1;
+
+    private double servo1Position;
+    private double servo2Position;
+
+    @Override
+    public void init() {
+        servo1 = hardwareMap.get(Servo.class, "rightServo");
+        servo2 = hardwareMap.get(Servo.class, "leftServo");
+
+        telemetry.addData("servo 1 position", servo1.getPosition());
+        telemetry.addData("servo 2 position", servo2.getPosition());
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
 }
